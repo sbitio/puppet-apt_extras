bf4aa4c93497d0c7fd7b111e5db98002613e529a
Pedro González Serrano <niteman.es@gmail.com>
2014-06-17 19:07:35 +0200

    added lts for squeeze https://wiki.debian.org/LTS/Using

---
+++ manifests/lts.pp
@@ -0,0 +1,13 @@
+class apt_extras::lts () {
+
+  Apt::Source <| title == 'lts' |> -> Package <| |>
+
+  if ( $::lsbdistcodename == "squeeze" ) {
+    apt::source { 'lts':
+      location          => "http://http.debian.net/debian/",
+      release           => "${::lsbdistcodename}-lts",
+      repos             => "main contrib non-free",
+      required_packages => "debian-keyring debian-archive-keyring",
+    }
+  }
+}
