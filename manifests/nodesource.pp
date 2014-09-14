38c25f510e1a7dcb34c18cc30dd02f68969a2e5c
Pedro González Serrano <niteman.es@gmail.com>
2014-09-14 21:41:30 +0200

    added nodesource

---
+++ manifests/nodesource.pp
@@ -0,0 +1,19 @@
+class apt_extras::nodesource () {
+
+  Apt::Source <| title == 'nodesource' |> -> Class['nodejs']
+
+  ensure_packages('apt-transport-https')
+
+  apt::key { "nodesource":
+    key        => "68576280",
+    key_source => "https://deb.nodesource.com/gpgkey/nodesource.gpg.key",
+    #require    => Package['apt-transport-https'],
+  }
+  apt::source { 'nodesource':
+    location          => "https://deb.nodesource.com/node",
+    release           => $::lsbdistcodename,
+    repos             => "main",
+    required_packages => "debian-keyring debian-archive-keyring",
+  }
+
+}
