c768fa6992e5630e36734e28481db95052781e43
Rodrigo Alfaro <rodricels@users.noreply.github.com>
2018-03-02 13:27:44 +0100

    Remove hyphen in mod-pagespeed.

--- manifests/modpagespeed.pp
+++ manifests/modpagespeed.pp
@@ -1,19 +1,19 @@
-# == Class: apt_extras::mod-pagespeed
+# == Class: apt_extras::modpagespeed
 #
-class apt_extras::mod-pagespeed {
+class apt_extras::modpagespeed {
 
   ensure_packages('apt-transport-https')
 
-  apt::key { 'mod-pagespeed':
+  apt::key { 'modpagespeed':
     key        => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
     source => 'https://dl.google.com/linux/linux_signing_key.pub',
   }
-  apt::source { 'mod-pagespeed':
+  apt::source { 'modpagespeed':
     location          => 'http://dl.google.com/linux/mod-pagespeed/deb/',
     release           => 'stable',
     repos             => 'main',
     required_packages => 'debian-keyring debian-archive-keyring',
-    require           => Apt::Key['mod-pagespeed'],
+    require           => Apt::Key['modpagespeed'],
   }
 
 }
